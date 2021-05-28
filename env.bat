0173e04c83f2c218e6fbe1321a2cd27e842a2727
owenh7 <77691097+owenh7@users.noreply.github.com>
2021-05-28 09:47:01 -0700

    Update env.bat

--- env.bat
+++ env.bat
@@ -1,4 +1,7 @@
-set MONGO_CONNECTION_STRING = pymongo.MongoClient("mongodb+srv://Owenh7:<iAaNJv04Igb9MaLT>@cluster0.jejgs.mongodb.net/test?retryWrites=true&w=majority")
+set MONGO_CONNECTION_STRING = pymongo.MongoClient("mongodb+srv://Owenh7:<iAaNJv04Igb9MaLT>@cluster0.jejgs.mongodb.net/test?retryWrites=true")
 set MONGO_DBNAME=testdb = client.test
+
+
+
 
 
